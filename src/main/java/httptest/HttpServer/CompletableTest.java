75765263304b00c72b90dfeaf7cfd6507a422614
daien <daien@bonc.com.cn>
2019-06-18 17:56:34 +0800

    add Junit

--- src/main/java/httptest/HttpServer/CompletableTest.java
+++ src/main/java/httptest/HttpServer/CompletableTest.java
@@ -1,15 +1,18 @@
 package httptest.HttpServer;
 
+import java.util.Random;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
+import java.util.function.BiConsumer;
 import java.util.function.Function;
 import java.util.function.Supplier;
+
+import org.junit.Test;
 
 public class CompletableTest {
 
     public static void main(String args[]) throws InterruptedException, ExecutionException {
-
         CompletableFuture<Integer> completableFuture = CompletableFuture.supplyAsync(new Supplier<String>() {
             public String get() {
                 try {
@@ -31,8 +34,40 @@ public class CompletableTest {
             }
         });
 
+        System.out.println("start....");
+
         Integer result = completableFuture.get();
         System.out.println(result);
+    }
+
+    @Test
+    public void whencomplete() throws InterruptedException {
+        CompletableFuture<Void> completableFuture = CompletableFuture.runAsync(new Runnable() {
+            public void run() {
+                try {
+                    TimeUnit.SECONDS.sleep(4);
+                } catch (InterruptedException e) {}
+                if (new Random().nextInt() % 2 >= 0) {
+                    int i = 12 / 0;
+                }
+            }
+        }).whenComplete(new BiConsumer<Void, Throwable>() {
+
+            public void accept(Void t, Throwable u) {
+                System.out.println("done");
+            }
+
+        }).exceptionally(new Function<Throwable, Void>() {
+
+            public Void apply(Throwable t) {
+                t.printStackTrace();
+                System.out.println("fail");
+                return null;
+            }
+        });
+
+        System.err.println("start..");
+        TimeUnit.SECONDS.sleep(5);
     }
 
 }
