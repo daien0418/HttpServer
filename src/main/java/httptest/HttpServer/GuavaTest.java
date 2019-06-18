892f432e6f6d54b588bc581bd47766bae78ca753
daien <daien@bonc.com.cn>
2019-06-18 09:20:26 +0800

    增加guava

---
+++ src/main/java/httptest/HttpServer/GuavaTest.java
@@ -0,0 +1,37 @@
+package httptest.HttpServer;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import com.google.common.util.concurrent.FutureCallback;
+import com.google.common.util.concurrent.Futures;
+import com.google.common.util.concurrent.ListenableFuture;
+import com.google.common.util.concurrent.ListeningExecutorService;
+import com.google.common.util.concurrent.MoreExecutors;
+
+public class GuavaTest {
+
+    public static void main(String args[]) {
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        ListeningExecutorService listeningExecutorService = MoreExecutors.listeningDecorator(executorService);
+        ListenableFuture<Integer> listenableFuture = listeningExecutorService.submit(new Callable<Integer>() {
+            public Integer call() throws Exception {
+                Thread.sleep(5000);
+                return 123;
+            }
+        });
+
+        Futures.addCallback(listenableFuture, new FutureCallback<Integer>() {
+
+            public void onFailure(Throwable arg0) {
+                // TODO Auto-generated method stub
+            }
+
+            public void onSuccess(Integer arg0) {
+                System.out.println(arg0);
+            }
+        }, listeningExecutorService);
+    }
+
+}
